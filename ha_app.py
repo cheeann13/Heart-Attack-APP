ad7f430d6ff7cd605cc3f4c6d07474cc0c8449fc
chee ann <low7nn@gmail.com>
2022-07-25 19:44:20 +0800

    Update ha_app.py

--- ha_app.py
+++ ha_app.py
@@ -75,39 +75,42 @@ with st.container():
         
     st.subheader('Please fill in the details of the person under consideration and click on the button below!')
     txt_col,img_col,  = st.columns((1, 2))
-    with txt_col:
-    with st.form("Heart Attack Predictor App"):
-        sex =           st.selectbox("Gender: (0 = Male ; 1 = Female)",(0,1))
-        age =           st.number_input("Age in Years", 1, 100, 25, 1)
-        chol  =         st.number_input("Serum Cholestoral in mg/dl",120,430)
-        thalachh =      st.slider('Maximum heart rate achieved', 50, 220, 1)
-        trtbps =        st.slider('Resting Blood Pressure', 60, 122, 69, 1)
-        oldpeak =       st.slider('ST Depression Induced by Exercise Relative to Rest', 0.0, 10.0, 0.01)
-        exng =          st.selectbox('Exercise Induced Angina (1 = yes; 0 = no)',(0,1))
-        thall =         st.selectbox('Thallium Stress Test(2 = normal; 1 = fixed defect; 3 = reversable defect)',(1,2,3))
-        caa =           st.selectbox('Number of Major Vessels (0-3) colored by flourosopy',(0,1,2,3))
-        cp =            st.selectbox('Chest Pain Type ( 0 = asymptomatic ; 1 = typical angina; 2 = atypical angina; 3 = non-anginal pain)',(0,1,2,3))
-        slp =           st.selectbox('The Slope of the Peak Exercise ST segment(0 = downsloping;1 = flat;2 = upsloping)',(0,1,2)) 
-        restecg =       st.selectbox('Resting Electrocardiographic Results (0 = hypertrophy;1 = normal; 2 = having ST-T wave abnormality)',(0,1,2))
-        
-        row = [age, sex, cp, trtbps, chol, restecg, thalachh, exng, oldpeak, slp, caa, thall]
-        
-        
-        submitted = st.form_submit_button("Analyse")
-        if submitted:
-            new_data=np.expand_dims(row,axis=0)
-            outcome=model.predict(new_data)[0]
-                                     
-            if outcome==0:
-                st.subheader('Anna says,')
-                st.title("Great! This patient is unlikely to get a heart attack!")
-                st.markdown("![Alt Text](https://bit.ly/anna-good)")
-            else:
-                st.subheader('Anna says,')
-                st.title("Oops! This patient has high risk of getting a heart attack! Please pay more attention.")
-                st.markdown("![Alt Text](https://bit.ly/p-sick)")
 
-                
+    with txt_col:
+        with st.form("Heart Attack Predictor App"):
+            sex =           st.selectbox("Gender: (0 = Male ; 1 = Female)",(0,1))
+            age =           st.number_input("Age in Years", 1, 100, 25, 1)
+            chol  =         st.number_input("Serum Cholestoral in mg/dl",120,430)
+            thalachh =      st.slider('Maximum heart rate achieved', 50, 220, 1)
+            trtbps =        st.slider('Resting Blood Pressure', 60, 122, 69, 1)
+            oldpeak =       st.slider('ST Depression Induced by Exercise Relative to Rest', 0.0, 10.0, 0.01)
+            exng =          st.selectbox('Exercise Induced Angina (1 = yes; 0 = no)',(0,1))
+            thall =         st.selectbox('Thallium Stress Test(2 = normal; 1 = fixed defect; 3 = reversable defect)',(1,2,3))
+            caa =           st.selectbox('Number of Major Vessels (0-3) colored by flourosopy',(0,1,2,3))
+            cp =            st.selectbox('Chest Pain Type ( 0 = asymptomatic ; 1 = typical angina; 2 = atypical angina; 3 = non-anginal pain)',(0,1,2,3))
+            slp =           st.selectbox('The Slope of the Peak Exercise ST segment(0 = downsloping;1 = flat;2 = upsloping)',(0,1,2)) 
+            restecg =       st.selectbox('Resting Electrocardiographic Results (0 = hypertrophy;1 = normal; 2 = having ST-T wave abnormality)',(0,1,2))
+
+            row = [age, sex, cp, trtbps, chol, restecg, thalachh, exng, oldpeak, slp, caa, thall]
+
+
+            submitted = st.form_submit_button("Analyse")
+                if submitted:
+                    new_data=np.expand_dims(row,axis=0)
+                    outcome=model.predict(new_data)[0]
+                                     
+                if outcome==0:
+                    st.subheader('Anna says,')
+                    st.title("Great! This patient is unlikely to get a heart attack!")
+                    st.markdown("![Alt Text](https://bit.ly/anna-good)")
+                else:
+                    st.subheader('Anna says,')
+                    st.title("Oops! This patient has high risk of getting a heart attack! Please pay more attention.")
+                    st.markdown("![Alt Text](https://bit.ly/p-sick)")
+
+    with img_col:
+        st.markdown("![Alt Text](https://bit.ly/anna-form)")
+        st.caption("Hold on, Anna is learning about the data"
                 
 # ---- CONTACT ----
 with st.container():
